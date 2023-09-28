3d2a2d5e1d03c5ce2a3e7719d3e41eaf4e4dfab2
lupescu141 <d.lupescu@hotmail.fi>
2023-09-28 13:10:02 +0300

    Changes by Janne

--- Main.py
+++ Main.py
@@ -19,16 +19,72 @@ try:
     yhteys = mysql.connector.connect(**tietokanta)
 
 except mysql.connector.errors.Error as err:
-
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
         print("Username or password is invalid")
-
     elif err.errno == errorcode.ER_BAD_DB_ERROR:
         print("Database does not exist")
-
     else:
         print(err)
 else:
     print("Connection: Succesful")
 
+#Päävalikon ohjaukseen
+def paavalikko():
+#Hakee txt tiedoston ja tulostaa päävalikon visuaalisen tekstin
+    for x in open(file="paavalikkoTeksti.txt"):
+        print(f"        {x}", end="")
+#Ottaa vastaan käyttäjän näppäin painalluksen ja toteuttaa tietyn funktion
+    while True:
 
+        if keyboard.is_pressed("1"):
+            luo_peli()
+            break
+
+        if keyboard.is_pressed("2"):
+            lataa_peli()
+            break
+
+        if keyboard.is_pressed("3"):
+            lataa_peli()
+            break
+
+
+#Tämä luo uuden tallennuksen tietokantaan eli uuden pelin
+def luo_peli():
+
+    while True:
+        nimi = input('Anna hahmollesi nimi: ')
+
+        if len(nimi) < 0:
+            print('Et voi antaa tyhjää nimeä.')
+
+        elif len(nimi) > 12:
+            print('Maksimi nimi on 12 merkkiä pitkä')
+
+        else:
+            break
+
+    sql = 'INSERT INTO peli (pelaaja_nimi)'
+    sql += f"VALUE ('{nimi}');"
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    return
+
+#Hakee viholliset tietokannasta
+def hae_viholliset():
+
+    sql = 'select * from viholliset'
+    kursori = yhteys.cursor(dictionary=True)
+    kursori.execute(sql)
+    lista = kursori.fetchall()
+    return lista
+
+#Lataa tallennuksen
+def lataa_peli():
+    return
+
+#Poistuu pelistä
+def poistu():
+    return
+
+luo_peli()
